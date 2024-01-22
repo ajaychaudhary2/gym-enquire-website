534928f2e4d01b5b1ececb860c5699e73aeb9677
Ajay chaudhary <ajaych2822@gmail.com>
2024-01-22 12:05:45 +0530

    gym website

---
+++ script.js
@@ -0,0 +1,19 @@
+const menu = document.querySelector(".navbar");
+const menuItems = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".ham");
+const closeIcon = document.querySelector(".closeicon");
+
+
+function toggleMenu() {
+    if 
+    }
+
+
+   
+}
+ham.addEventListener("click", toggleMenu);
+menuItems.forEach(
+    function (menuItem) {
+        menuItem.addEventListener("click", toggleMenu);
+    }
+)
